ee3a758cb79a0de65e90a9c1ea09f01a6e976d91
sahbaniB <sahbenibouchra68@gmail.com>
2023-02-06 15:58:10 +0100

    add warehouse for SAV

---
+++ Projet_vrp/Autonomous_vehicle/Warehouses.py
@@ -0,0 +1,11 @@
+class WareHouse:
+    def __init__(self, warehouse_ID, warehouse_position, numberofSAV, listofSAV) -> None:
+        self.warehouse_ID = warehouse_ID
+        self.warehouse_position = warehouse_position
+        self.numberofSAV = numberofSAV
+        self.listofSAV = listofSAV
+
+    def update_warehouse(self):
+        pass
+
+    

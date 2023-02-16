204e7f52563089506a8c23355fda1ed7253eb11b
SAHBANI <sahbenibouchra68@gmail.com>
2023-02-16 14:07:52 +0100

    correct the update_warehouse function

--- Projet_vrp/Autonomous_vehicle/Warehouses.py
+++ Projet_vrp/Autonomous_vehicle/Warehouses.py
@@ -1,3 +1,18 @@
+import logging 
+
+#now we will Create and configure logger 
+logging.basicConfig(filename="C:/GIT_VRP/VRP_SAV/Projet_vrp/logfile.log", 
+					format='%(asctime)s %(message)s', 
+					filemode='w',
+                    level=logging.INFO) 
+
+
+#Let us Create an object 
+logger=logging.getLogger() 
+
+#Now we are going to Set the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
+    
 class WareHouse:
     def __init__(self, warehouse_ID, warehouse_position, numberofSAV, listofSAV) -> None:
         self.warehouse_ID = warehouse_ID
@@ -5,7 +20,9 @@ class WareHouse:
         self.numberofSAV = numberofSAV
         self.listofSAV = listofSAV
 
-    def update_warehouse(self):
-        pass
+    def update_warehouse(self, listofSAV):
+        for SAV in listofSAV:
+            #self.listofSAV.append(SAV)
+            logging.info("SAV {} is added to the Warehouse {} with the position {}".format(SAV.SAV_ID, self.warehouse_ID, self.warehouse_position))
 
     

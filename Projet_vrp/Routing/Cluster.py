e1316b7ded36a0f1dde2d8afbc450122f1ceb905
sahbaniB <sahbenibouchra68@gmail.com>
2023-02-23 16:34:35 +0100

    add routing cluster

---
+++ Projet_vrp/Routing/Cluster.py
@@ -0,0 +1,8 @@
+class Cluster:
+    def __init__(self, cluster_id,listofpassenger, refrencepassenger):
+        self.cluster_id = cluster_id
+        self.listofpassenger = listofpassenger
+        self.refrencepassenger = refrencepassenger 
+    
+    def definethelistpassenger():
+        pass
